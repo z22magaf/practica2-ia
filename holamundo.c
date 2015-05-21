3f9c3a66b36ab184b8f6101612cc9de429d5c2a5
z22magaf <z22magaf@uco.es>
2015-05-21 23:53:34 +0200

    hecho un cambio

--- holamundo.c
+++ holamundo.c
@@ -1,6 +1,6 @@
 # include "./includes/miinclude.h"
 int main(){
 char c[100]= "Hola mundo";
-printf("%s\n", strcat(c,", soy i22lojal"));
+printf("%s\n", strcat(c,", soy i22lojalsdfsdfsfds"));
 return 0;
 }
